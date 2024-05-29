bba975e1bf3bb78e30d2f07e18f36e313d4074f7
Jessica <jessica_costaignacio@hotmail.com>
2024-05-29 13:49:16 -0300

    Atualiza o posicionamento do texto e da imagem na página Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,24 +1,20 @@
-
 function Home() {
-    return (
-        <>
-        <div className='h-screen bg-cyan-200 flex justify-center'>
-          <div className='container grid grid-cols-2 text-black'>
-            <div className='flex flex-col gap-4 items-center justify-center py-4'>
-              <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
-              <p className='text-lg'>Aqui você encontra Medicamentos e Cosméticos.</p>
-            </div>
-          </div>
-          <div className="flex justify-center items-center">
-                <img src="https://ik.imagekit.io/eijess/img.png?updatedAt=1716989544128" 
-                alt="Imagem da Página Home" 
-                className="h-3/3"
-                />
-            </div>
+  return (
+    <div className='h-screen bg-cyan-200 flex justify-center items-center'>
+      <div className='container grid grid-cols-2 text-black'>
+        <div className='flex flex-col justify-center py-4 pl-16'>
+          <h2 className='text-5xl font-bold'>Seja bem vindo!</h2>
+          <p className='text-lg'>Aqui você encontra Medicamentos e Cosméticos.</p>
         </div>
-      
-      </>
-    );
+        <div className="flex justify-center items-center">
+          <img src="https://ik.imagekit.io/eijess/img.png?updatedAt=1716989544128" 
+            alt="Imagem da Página Home" 
+            className="h-3/4"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
