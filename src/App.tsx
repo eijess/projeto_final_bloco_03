4b4d7deced0e3695fc5cd359d787bebc9f808ba7
Jessica <jessica_costaignacio@hotmail.com>
2024-05-29 12:36:01 -0300

    CRUD Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Footer from './components/footer/Footer';
-import Navbar from './components/categorias/navbar/Navbar';
-import Home from './home/Home';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
